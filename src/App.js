bbd4293ef4e143871420dbcc7d1aa5af90d1bb96
Harrish <haryeshayer@gmail.com>
2022-07-28 13:25:22 +0545

    next commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+//import pages in App.js
+
+// import { BrowserRouter,  Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./booking/Home";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import TopNav from "./components/TopNav";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <TopNav/>
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+          <Route  exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        
+      </Routes>
+
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ src/booking/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container-fluid h1 p-5 text-center'>
+        HOMEPAGE
+    
+    </div>
+  )
+}
+
+export default Home
